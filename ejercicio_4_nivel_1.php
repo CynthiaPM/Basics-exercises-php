d7f261572f3c5fadd84a33c87765ce3dadc9dfd6
CynthiaPM <cynthiaeugeniapm@gmail.com>
2023-01-24 21:57:53 +0100

    ejercicio 5 listo

---
+++ ejercicio_4_nivel_1.php
@@ -0,0 +1,30 @@
+<?php
+
+$X = 3;
+$Y = 2;
+$N = 3.5;
+$M = 2.5;
+
+echo "El valor de X es = $X </br>
+el valor de Y es = $Y </br>";
+echo "suma = ".$X+$Y ."</br>";
+echo "resta = ".$X-$Y ."</br>";
+echo "producto = ".$X*$Y ."</br>";
+echo "modulo = ".$X%$Y ."</br>"; 
+
+echo "El valor de N es = $N </br>
+el valor de M es = $M </br>";
+echo "suma = ".$N+$M ."</br>";
+echo "resta = ".$N-$M ."</br>";
+echo "producto = ".$N*$M ."</br>";
+echo "modulo = ".$N%$M ."</br>"; 
+
+echo "el doble de X es =".$X*2.."</br>";
+echo "el doble de Y es =".$Y*2.."</br>";
+echo "el doble de N es =".$N*2.."</br>";
+echo "el doble de M es =".$M*2.."</br>";
+
+echo "la suma de todas las variables es = ".$X+$Y+$N+$M."</br>";
+
+echo "el producto de todas las variables es = ".$X*$Y*$N*$M;
+?>
