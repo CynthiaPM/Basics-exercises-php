a558a4347b1660ba10fe7f9bbf5b4c1d31f75668
CynthiaPM <cynthiaeugeniapm@gmail.com>
2023-01-25 11:28:05 +0100

    ejercicio 2 terminado

--- ejercicio_2_nivel_1.php
+++ ejercicio_2_nivel_1.php
@@ -1,3 +1,18 @@
 <?php 
+    $saludo = "Hello, World!";
+
+    echo $saludo ."</br>";
+
+    $saludo2 = strtoupper($saludo);
+
+    echo strlen($saludo)."</br>";
+    
+    echo $saludo2 ."</br>";
+
+    echo strrev($saludo)."</br>";
+
+    $curso = "Este es el curso de PHP";
+
+    echo $saludo.$curso;
 
 ?>
